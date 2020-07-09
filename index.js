03e896db3ed247b2e411ed1c1c90b4ad08c2a0c4
Shruthi <shruthip15.sk@gmail.com>
2020-07-09 22:22:12 +0530

    Fixed navigation bar

---
+++ index.js
@@ -0,0 +1,10 @@
+function displayHome() {
+    document.getElementById("cart").style.display = "none";
+    document.getElementById("home").style.display = "block";
+
+}
+function displayCart() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("cart").style.display = "block";
+
+}
