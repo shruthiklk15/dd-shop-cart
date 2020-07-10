6d4c70cc4d585be58cf54bfd8584d73ea1a81133
Shruthi <shruthip15.sk@gmail.com>
2020-07-10 15:37:58 +0530

    Restricted img in home and Font style changed.

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function displayCart() {
     $("#home").hide();
     $("#cart").show();
 }
+
+function drag(event) {
+    // console.log(event.id);
+    $("#" + event.id).draggable();
+    $("#home Img").draggable({
+        containment: "#home"
+    });
+
+}
+
+
